982a9676f4752f2663026d569dc58bf6e31046b1
Camel2548 <Camelmlgxd@gmail.com>
2026-04-26 10:48:06 +0000

    camallux

--- main.cpp
+++ main.cpp
@@ -1,70 +1,82 @@
-#include <iostream>
+#include <iostream> //kub
 #include <exception>
-#include <cmath>
-#include <limits>
-#include <new>
-
 using namespace std;
+void check0(int );
+void checkFail();
+void check1000(int ,int);
 
-
-class InputFailException : public exception {
-public:
-    const char* what() const throw() { return "Incorrect numbers"; }
+class div0:public exception{
+	public:
+	virtual const char* what() const throw(){
+		return "Error divide by zero";
+	}
 };
 
-class Div0Exception : public exception {
-public:
-    const char* what() const throw() { return "Divided by zero"; }
+class input_fail:public exception{
+	public:
+	virtual const char* what() const throw(){
+		return "Incorrect type entered";
+	}
 };
 
-class RangeException : public exception {
-public:
-    const char* what() const throw() { return "Value out of range"; }
+class range1000:public exception{
+	public:
+	virtual const char* what() const throw(){
+		return "Value out of range (-10000 to 1000)";
+	}
 };
 
-void checkDivisor(int y) {
-    if (y == 0) throw Div0Exception();
+int main(){
+	int x,y;
+	double d;
+	int a=1;
+do{
+	try{
+
+		cout<<"Enter 2 num: ";
+		cin>>x>>y;
+		checkFail();
+		check1000(x,y);
+		check0(y);
+		d=(double) x/y;
+		cout<< "The result is " <<d<<endl;
+		a=0;
+		int i ;
+		double* myarray;
+		for(i=0;i<100000;i++){
+			cout<<"Allocating memory ...."<<i<<endl;
+			myarray= new double[5000000000];
+		}
+	}
+
+	catch(exception& e){
+		cout<<e.what()<<endl;
+		
+	}
+}while(a);
+	return 0;
 }
 
-int main() {
-    int x, y;
-    double d;
+void check0(int x){
+	div0 e;
+	if(x==0){
+		throw e;
+	}
+}
 
-    while (true) { 
-        try {
-            cout << "Enter 2 numbers: ";
-
-            if (!(cin >> x >> y)) throw InputFailException();
-            if (abs(x) > 1000 || abs(y) > 1000) throw RangeException();
-            checkDivisor(y);
-
-   
-            d = (double)x / y;
-            cout << "The result is " << d << endl;
-            
-          
-            for (int i = 0; i < 1; i++) {
-                cout << "Allocating memory .... " << i << endl;
-                
-                double* myarray = new double[999999999999999]; 
-            }
-
-            break; 
-
-        } 
-        catch (const exception &e) {
-    
-            cout << e.what() << endl;
-
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                cout << "Please try again...\n" << endl;
-            } else {
-             
-                return 0;
-            }
-        }
+void checkFail() {
+	input_fail e;
+    if (cin.fail()) {
+		cin.clear();
+		cin.ignore(50,'\n');
+        throw e;
     }
-    return 0;
+}
+
+void check1000(int x,int y){
+	range1000 e;
+	if (x < -10000 || x > 1000 || y < -10000 || y > 1000) {
+			throw e;
+        }
+
 }
